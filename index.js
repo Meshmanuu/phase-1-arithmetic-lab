1aeb60a54153400799430cf5dec55aa2d7d8088d
Meshmanuu <meshanyangu@gmail.com>
2024-03-24 15:59:52 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+// Challenge 1: Multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// Challenge 2: Random
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Challenge 3: Mod
+let num3 = 14; // Change num3 value to 14
+let num4 = 5;  // Change num4 value to 5
+let mod = num3 % num4;
+
+// Challenge 4: Max
+let max = Math.max(1, 2, 3, 4, 5, 10, 15, 20, 15);
+
+// Testing the variables
+console.log("Challenge 1 - Multiply:", multiply); // Should output 62
+console.log("Challenge 2 - Random:", random); // Should output a random number greater than 0
+console.log("Challenge 3 - Mod:", mod); // Should output 4
+console.log("Challenge 4 - Max:", max); // Should output 20
+
